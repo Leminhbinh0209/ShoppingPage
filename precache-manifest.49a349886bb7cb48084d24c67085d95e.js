f1df49e3fec3983e7eb135dfb00ffdc8d2518734
Leminhbinh0209 <binhleminh0209@gmail.com>
2020-07-06 01:13:17 -0600

    Updates

---
+++ precache-manifest.49a349886bb7cb48084d24c67085d95e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb203ccd706aad2458909131e0b77d59",
+    "url": "/ShoppingPage/index.html"
+  },
+  {
+    "revision": "1a1b4c78f963e64cbbba",
+    "url": "/ShoppingPage/static/css/main.23b1b92a.chunk.css"
+  },
+  {
+    "revision": "d22cee2381177f366aeb",
+    "url": "/ShoppingPage/static/js/2.4bfe4e98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShoppingPage/static/js/2.4bfe4e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1b4c78f963e64cbbba",
+    "url": "/ShoppingPage/static/js/main.d015436c.chunk.js"
+  },
+  {
+    "revision": "8b636c35a4836b33c7eb",
+    "url": "/ShoppingPage/static/js/runtime-main.d61902f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ShoppingPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
